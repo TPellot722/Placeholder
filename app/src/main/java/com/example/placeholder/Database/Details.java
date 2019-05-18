2bffef03b521f2675a38e50abb64869fec5134a1
Tristen Pellot <tristenpellot22@gmail.com>
2019-05-18 14:18:09 -0400

    created Details class

--- app/src/main/java/com/example/placeholder/Database/Details.java
+++ app/src/main/java/com/example/placeholder/Database/Details.java
@@ -5,6 +5,9 @@ public class Details {
     private String itemName;
     private double price;
 
+    public Details() {
+    }
+
     public Details(int orderID, String itemName, double price) {
         this.orderID = orderID;
         this.itemName = itemName;
